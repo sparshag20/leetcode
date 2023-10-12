b71116767098dfac10ec8b057b679d4ca834651c
sparshag20 <93657989+sparshag20@users.noreply.github.com>
2023-10-12 16:06:28 +0530

    Time: 2 ms (65.52%), Space: 40.1 MB (75.61%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -5,8 +5,7 @@ class Solution {
         int max=0;
         for(int i=0;i<n;i++){
             if(citations[i]>=n-i){
-                max=Math.max(max,n-i);
-                return max;
+                return n-i;
             }
         }
         return 0;
